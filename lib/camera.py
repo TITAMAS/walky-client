c312aa594f5c4ab53d427265f3cd78f565deae77
sh8 <s@sh8.io>
2017-06-25 13:47:30 +0900

    Open camera with `with` statement

--- lib/camera.py
+++ lib/camera.py
@@ -13,19 +13,18 @@ import picamera
 
 def snapshot(name):
     """スナップショットを撮影"""
-    camera = picamera.PiCamera()
-    filepath = os.path.join(os.getcwd(), 'images', name)
-    camera.hflip = True
-    camera.vflip = True
-    camera.resolution = (640, 480)
-    camera.shutter_speed = 5000
-    camera.iso = 800
-    try:
-        camera.capture(filepath, format='jpeg')
-    except PiCameraError:
-        print('Something went wrong with the camera')
-        time.sleep(3)
-        snapshot(name)
+    with picamera.PiCamera() as camera:
+        filepath = os.path.join(os.getcwd(), 'images', name)
+        camera.hflip = True
+        camera.vflip = True
+        camera.resolution = (640, 480)
+        camera.shutter_speed = 5000
+        camera.iso = 800
+        try:
+            camera.capture(filepath, format='jpeg')
+        except PiCameraError:
+            print('Something went wrong with the camera')
+            time.sleep(3)
 
 
 if __name__ == '__main__':
