a5998a31d8dbaebfafab914451542448f8368d30
sh8 <s@sh8.io>
2017-07-17 22:35:02 +0900

    Fix sonic_sensor.py

--- lib/sonic_sensor.py
+++ lib/sonic_sensor.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import time
+
 def read_distance():
     """加速度センサから距離を読み取る"""
 
     # 必要なライブラリのインポート・設定
-    import time
     import RPi.GPIO as GPIO
 
     try:
@@ -17,10 +18,9 @@ def read_distance():
         ECHO = 27 # ボード上の13番ピン(GPIO27)
 
         # ピンのモードをそれぞれ出力用と入力用に設定
-        GPIO.setup(TRIG,GPIO.OUT)
-        GPIO.setup(ECHO,GPIO.IN)
+        GPIO.setup(TRIG, GPIO.OUT)
+        GPIO.setup(ECHO, GPIO.IN)
         GPIO.output(TRIG, GPIO.LOW)
-        time.sleep(0.3)
 
         # TRIGに短いパルスを送る
         GPIO.output(TRIG, GPIO.HIGH)
@@ -28,20 +28,28 @@ def read_distance():
         GPIO.output(TRIG, GPIO.LOW)
 
         # ECHO ピンがHIGHになるのを待つ
+        signaloff = time.time()
         while GPIO.input(ECHO) == GPIO.LOW:
             signaloff = time.time()
 
         # ECHO ピンがLOWになるのを待つ
-        while GPIO.input(ECHO) == GPIO.HIGH:
-            signalon = time.time()
+        signalon = signaloff
+        while time.time() < signaloff + 0.1:
+            if GPIO.input(ECHO) == GPIO.LOW:
+                signalon = time.time()
+                break
 
         # 時刻の差から、物体までの往復の時間を求め、距離を計算する
         timepassed = signalon - signaloff
         distance = timepassed * 170
-        GPIO.cleanup()
+
         return distance
-    except:
+
+    except Exception as e:
+        print(e)
         return 0.0
+    finally:
+        GPIO.cleanup()
 
 if __name__ == '__main__':
     dist = read_distance()
