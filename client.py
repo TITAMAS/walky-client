e3835339b757e35c185bbd8198090ba7fdfa3055
sh8 <s@sh8.io>
2017-07-19 16:03:22 +0900

    Upload images to S3 when debug mode

--- client.py
+++ client.py
@@ -14,6 +14,7 @@ from lib.dip import read_switch
 from lib.filtering import filter_tags
 from lib.sonic_sensor import read_distance
 from lib.recognition import recognize_image
+from lib import settings
 from lib.speak import speak, speak_raw
 
 DEBUG_PIN = 26
@@ -84,6 +85,17 @@ if __name__ == '__main__':
                     end = time.time()
                     print("end:", end)
             else:
+                from boto.s3.connection import S3Connection
+                from boto.s3.key import Key
+
+                BUCKET_NAME = 'walky-debug'
+
+                conn = S3Connection(
+                        aws_access_key_id=settings.AWS_ACCESS_KEY,
+                        aws_secret_access_key=settings.AWS_SECRET_KEY)
+
+                bucket = conn.get_bucket(BUCKET_NAME)
+
                 directory = 'images'
                 if not os.path.exists(directory):
                     os.makedirs(directory)
@@ -92,6 +104,16 @@ if __name__ == '__main__':
                 filepath = os.path.join(os.getcwd(), 'images', 'image.jpg')
                 snapshot(camera, filepath)
                 speak_raw('finish taking a picture', LANG)
+
+                key = Key(bucket)
+                key.key = filepath
+                key.set_contents_from_filename(filepath)
+
+                # Make images public
+                key.make_public()
+
+                speak_raw('Uploaded', LANG)
+
                 time.sleep(3)
 
         camera.stop_preview()
